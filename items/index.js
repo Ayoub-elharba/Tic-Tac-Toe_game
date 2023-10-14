a7bd46594f03cc7c1387ec4361acb2f0fb753a1d
Ayoub-elharba <elharba.ayoub@ofppt-edu.ma>
2023-10-14 21:03:27 +0100

    tic tac toe

---
+++ items/index.js
@@ -0,0 +1,102 @@
+// FOOTER
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById('years').innerHTML=year;
+
+// AUDIO PLAYER
+var x = document.getElementById("audio1"); 
+
+function playAudio() { 
+  x.play(); 
+} 
+
+function pauseAudio() { 
+  x.pause(); 
+} 
+
+// BUTTON RESET
+function reset(){
+    location.reload();
+}
+
+// ALERT ICON
+Swal.fire('YOU WELCOME!')
+let title=document.querySelector('.title');
+let turn = 'x';
+let squares=[];
+var y = document.getElementById("audio0"); 
+
+function end(num1,num2,num3)
+{
+    document.querySelector('body').style.pointerEvents = "none";
+    y.play();
+    title.innerHTML = `${squares[num1]} Winner`;
+    document.getElementById('item'+num1).style.background='#000';
+    document.getElementById('item'+num2).style.background='#000';
+    document.getElementById('item'+num3).style.background='#000';
+    setInterval(function(){title.innerHTML += '.'},1000);
+    setTimeout(function(){location.href='../index.html'},7000);
+    
+
+}
+function winner()
+{
+    for (let i=1 ;i<10;i++)
+    {
+        squares[i]= document.getElementById('item' +i).innerHTML;
+    }
+    if(squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != '')
+    {
+        end(1,2,3);
+    }
+    else if(squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != '')
+    {
+        end(4,5,6);
+
+    }
+    else if(squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != '')
+    {
+        end(7,8,9);
+    }
+    else if(squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != '')
+    {
+        end(1,4,7);
+        
+    }
+    else if(squares[2] == squares[5] && squares[5] == squares[8] && squares[5] != '')
+    {
+        end(2,5,8);
+    }
+    else if(squares[3] == squares[6] && squares[6] == squares[9] && squares[6] != '')
+    {
+        end(3,6,9);
+    }
+    else if(squares[1] == squares[5] && squares[5] == squares[9] && squares[5] != '')
+    {
+        end(1,5,9)
+    }
+    else if(squares[3] == squares[5] && squares[5] == squares[7] && squares[5] != '')
+    {
+        end(3,5,7)
+        
+    }
+  
+}
+
+function game(id)
+{
+    let element = document.getElementById(id);
+    if(turn === 'x' && element.innerHTML == '')
+    {
+        element.innerHTML = 'X';
+        turn ='o';
+        title.innerHTML='O';
+    }
+    else if(turn === 'o' && element.innerHTML == '')
+    {
+        element.innerHTML = 'O';
+        turn ='x';
+        title.innerHTML='X';
+    }
+    winner();
+}
---
+++ main.js
@@ -0,0 +1,13 @@
+
+window.onload=function(){
+    document.getElementById('audio1').play()
+}
+var x = document.getElementById("audio1"); 
+
+function playAudio() { 
+x.play(); 
+} 
+
+function pauseAudio() { 
+x.pause(); 
+} 
